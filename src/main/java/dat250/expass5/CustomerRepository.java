6710f327afe96983cf040e7a4459f8eb76ca82ec
EndreSletnes <endre.sletne@gmail.com>
2023-10-03 15:25:50 +0200

    Initial commit

---
+++ src/main/java/dat250/expass5/CustomerRepository.java
@@ -0,0 +1,11 @@
+package dat250.expass5;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
---
+++ src/test/java/dat250/expass5/Expass5ApplicationTests.java
@@ -0,0 +1,13 @@
+package dat250.expass5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expass5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
